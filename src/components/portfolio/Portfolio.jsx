fce883ac8701ff1574a2594682d48fe634b09450
hafijur1238 <saadmalik12349@gmail.com>
2024-10-07 12:55:25 +0600

    complete portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
-import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { useRef } from "react";
-import { motion, useScroll, useSpring } from "framer-motion";
+import "./portfolio.scss";
 
 const items = [
     {
@@ -12,7 +12,7 @@ const items = [
     {
         id: 2,
         title: "Next.js Blog",
-        img: "https://images.pexels.com/photos/28397252/pexels-photo-28397252/free-photo-of-dazzling-star-trails-over-dark-forest.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=600",
         desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, eligendi officia? Quisquam explicabo quasi ratione voluptatibus ipsa sit aperiam cum incidunt, ut eveniet facere recusandae vel, ullam maxime quia repellat.",
     },
     {
@@ -30,9 +30,30 @@ const items = [
 ];
 
 const Single = ({ item }) => {
+
+    const ref = useRef();
+
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        // offset: ["start start", "end start"]
+    });
+
+    const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
     return (
-        <section>
-            {item.title}
+        <section >
+            <div className="container">
+                <div className="wrapper">
+                    <div className="imageContainer" ref={ref}>
+                        <img src={item.img} alt="" />
+                    </div>
+                    <motion.div className="textContainer" style={{ y }}>
+                        <h2>{item.title}</h2>
+                        <p>{item.desc}</p>
+                        <button>See Demo</button>
+                    </motion.div>
+                </div>
+            </div>
         </section>
     )
 }
